74c1473818e6a13c360572b4747eae546a6b960e
ziad <ziodnaser33@gmail.com>
2025-05-19 13:58:15 +0300

    apply changes

---
+++ Ecommerse_Project.Api/MiddleWare/ApiException.cs
@@ -0,0 +1,16 @@
+﻿namespace Ecommerce__Project.Api.MiddleWare
+{
+    public class ApiException
+    {
+        public int StatusCode { get; }
+        public string Message { get; }
+        public string? Details { get; }
+
+        public ApiException(int statusCode, string message, string? details = null)
+        {
+            StatusCode = statusCode;
+            Message = message;
+            Details = details;
+        }
+    }
+}
---
+++ Ecommerse_Project.DAL/Models/BaserEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerse_Project.DAL.Models
+{
+    public class BaserEntity
+    {
+        public int Id { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime? UpdatedAt { get; set; }
+
+        public bool IsDeleted { get; set; } = false;
+
+        public DateTime? DeletedAt { get; set; }
+    }
+}
